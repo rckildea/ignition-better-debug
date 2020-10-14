e9c5c48fe962faf1d8c7a074f462109a23e4987f
Ryan Kildea <rckildea@gmail.com>
2020-10-14 17:18:06 -0500

    gaesrg

--- resources/js/components/Tab.vue
+++ resources/js/components/Tab.vue
@@ -2,6 +2,12 @@
     <div class="tab-content">
         <div class="layout-col">
             <h2>{{ meta.title }}</h2>
+            <input type="checkbox" id="jack" value="Jack" v-model="checkedNames">
+            <label for="jack">Jack</label>
+            <input type="checkbox" id="john" value="John" v-model="checkedNames">
+            <label for="john">John</label>
+            <input type="checkbox" id="mike" value="Mike" v-model="checkedNames">
+            <label for="mike">Mike</label>
         </div>
     </div>
 </template>
